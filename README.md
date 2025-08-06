1f269879dd513250a64a5b795f25a4b2e103dffe
aleksandra-lisek <olalisek.al@gmail.com>
2025-08-06 10:43:45 +0200

    initial setup

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # hello-mars
+
+## Example usage
+
+```yaml
+uses: actions/hello-mars-javascript-action@e76147da8e5c81eaf017dede5645551d4b94427b
+with:
+  name: Mona the Octocat
+```
--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 import *  as core from '@actions/core';
 import *  as core from '@actions/github';
 
-const name = core.getInput('name');
+try {
+  const name = core.getInput('name');
   const outPutValue = `Hello ${nameToGreet} from Mars!`;
   core.setOutput('greeting', outPutValue);
+} catch (error) {
+  core.setFailed(error.message);
+}
